b15ea70006bdfb401e9c56939c30ae17fee5b934
Ольга1213 <O_Mischenkova@mail.ru>
2024-07-16 21:03:08 +0500

    домашка9

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,75 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        //1
+        int[] nums = new int[3];
+        double[] doubles = {1.57, 7.654, 9.9};
+        boolean[] bools = {true, false, true};
+
+        //2
+
+        for (int i = 0;
+             i < nums.length; i++) {
+            System.out.print(nums[i]);
+            if (i < nums.length - 1) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < doubles.length; i++) {
+            System.out.print(doubles[i]);
+            if (i < doubles.length - 1) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < bools.length; i++) {
+            System.out.print(bools[i]);
+            if (i < bools.length - 1) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+
+        //3
+        for (int i = nums.length - 1;
+             i >= 0; i--) {
+            System.out.print(nums[i]);
+            if (i > 0) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+        for (int i = doubles.length - 1;
+             i >= 0; i--) {
+            System.out.print(doubles[i]);
+            if (i > 0) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+        for (int i = bools.length - 1;
+             i >= 0; i--) {
+            System.out.print(bools[i]);
+            if (i > 0) {
+                System.out.print(",");
+            }
+        }
+
+        //4
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 != 0) {
+                nums[i]++;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]);
+            if (i < nums.length - 1) {
+                System.out.print(",");
+            }
         }
     }
 }
